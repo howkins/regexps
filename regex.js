c2b838094560d918731bdcedaedbf3fac70afd36
Georgi Zhelezov <howkinss@gmail.com>
2017-05-10 12:00:49 +0300

    removed comment

--- regex.js
+++ regex.js
@@ -10,7 +10,6 @@ regex = function () {
     switch(type){
       case 'integer': pattern = /^\d+$/;break;
       case 'float': pattern = /^[-+]?[0-9]*\.[0-9]+([eE][-+]?[0-9]+)?$/;break;
-      /*Email will be change with another logic*/
       case 'email': pattern = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/;break;
       case 'date': pattern = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;break;
       case 'range':
