58ddfd4c855ab6509c25f97d66eaaeb00c6d0e0c
Georgi Zhelezov <howkinss@gmail.com>
2017-05-10 12:25:24 +0300

    delete all comments

--- regex.js
+++ regex.js
@@ -27,10 +27,6 @@ regex = function () {
           }
         }
         break;
-      // first comment from branch future_dayofweek
-      // first master commit line
-      // second line future_dayofweek
-      // 3..
       case 'dayofweek':
         pattern = /^(sun|sunday|mon|monday|tue|tuesday|wed|wednesday|thu|thursday|fri|friday|sat|saturday)$/gi
         break;
